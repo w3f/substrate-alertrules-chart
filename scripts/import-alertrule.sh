3f06d202a76282f1f86a0fc9fdfd5038e1a9f062
ironoa <alessio.onori@gmail.com>
2020-09-01 23:39:33 +0200

    test

--- scripts/import-alertrule.sh
+++ scripts/import-alertrule.sh
@@ -8,7 +8,7 @@ import_alertrule() {
   cat ${CURRENT_DIR}/template/prefix.yaml > ${RULE_FILE_NAME}
   yq p ${SUBSTRATE_RULES_FOLDER}/alerting-rules.yaml spec | sed 's/{{ $labels.instance }}/{{`{{ $labels.instance }}`}}/g' - | sed 's/{{ $value }}/{{`{{ $value }}`}}/g' - | yq w - "spec.groups[*].rules[*].labels.origin" "{{ .Values.deploymentName }}" | sed "s/'{{ .Values.deploymentName }}'/{{ .Values.deploymentName }}/g" - >> ${RULE_FILE_NAME}
   cat ${CURRENT_DIR}/template/postfix.yaml >> ${RULE_FILE_NAME}
-  mv ${RULE_FILE_NAME} ${PROJECT_ROOT}/${CHART_FOLDER}
+  mkdir -p ${PROJECT_ROOT}/${CHART_FOLDER} && mv ${RULE_FILE_NAME} ${PROJECT_ROOT}/${CHART_FOLDER}
 }
 
 print_imported() {
