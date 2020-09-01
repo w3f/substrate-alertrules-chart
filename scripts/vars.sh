5c0489967f25a59efaad099c876beac57b4348e5
ironoa <alessio.onori@gmail.com>
2020-09-01 23:51:28 +0200

    code refactoring

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 SUBSTRATE_VER=v2.0.0-rc6
 PROJECT_ROOT=`git rev-parse --show-toplevel`
-TEST_FOLDER="tests/prometheus/substrate-alertrules"
+TEST_FOLDER_PROMETHEUS="tests/prometheus"
+TEST_FOLDER="${TEST_FOLDER_PROMETHEUS}/substrate-alertrules"
 CHART_FOLDER="charts/substrate-alertrules/templates"
 RULE_FILE_NAME="alertrules.yaml"
 TEST_FILE_NAME="alertrules.yaml"
